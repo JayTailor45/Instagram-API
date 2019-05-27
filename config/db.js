6aea5bfe6812d7ccbefe5499de713fe670e64039
Jay Tailor <tailorj64@gmail.com>
2019-05-27 13:35:35 +0530

    Refactored server setup code

--- config/db.js
+++ config/db.js
@@ -1,9 +1,18 @@
 const Sequalize = require('sequelize');
 
-exports.db = new Sequalize('Instagram', 'root', '', {
-    host: 'localhost',
-    port: 3306,
-    dialect: 'mysql'
+////////// Setup Server ////////////
+const DATABASE_NAME = 'Instagram';
+const USERNAME      = 'root';
+const PASSWORD      = '';
+const HOSTNAME      = 'localhost';
+const PORT          = 3333;
+const DB_TYPE       = 'mysql'
+/////////////////////////////////////
+
+exports.db = new Sequalize(DATABASE_NAME, USERNAME, PASSWORD, {
+    host: HOSTNAME,
+    port: PORT,
+    dialect: DB_TYPE,
 });
 
 if (exports.db) {
